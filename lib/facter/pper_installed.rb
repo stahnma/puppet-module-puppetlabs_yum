d5067622b143ab30b2716d15b5aab0198246d7f6
Michael Stahnke <stahnma@puppetlabs.com>
2013-08-07 07:07:43 +0000

    Ensure that puppetlabs_yum doesn't do crazy things on top of PE.
    Prior to this commit, runnign this module on a system running Puppet Enterprise
    would just install the FOSS repos. This can lead to unpredictable behavior for
    PE users, and generally non-goodness.
    
    This commit creates a custom fact to determine if pe-puppet-enterprise-release
    is installed. If it is, we just noop the module. This allows for all systems to
    have this modules setup and classified while still retraining relatively sane
    and expected behavior.

---
+++ lib/facter/pper_installed.rb
@@ -0,0 +1,13 @@
+# This fact determines if you are running Puppet Enterprise on an RPM based
+# system.
+
+Facter.add(:pper_installed) do
+  setcode do
+    %x{rpm -q pe-puppet-enterprise-release &> /dev/null}
+    if $? == 0
+      true
+    else
+      false
+    end
+  end
+end
--- manifests/init.pp
+++ manifests/init.pp
@@ -14,27 +14,33 @@ class puppetlabs_yum (
   $enable_devel = false
 ) inherits puppetlabs_yum::params {
 
-  if $::osfamily == 'RedHat' {
-    include puppetlabs_yum::products
-    include puppetlabs_yum::deps
-    class { 'puppetlabs_yum::devel':
-      enable_devel   => $enable_devel,
-    }
+  if $::pper_installed == true {
 
-    puppetlabs_yum::rpm_gpg_key{ 'RPM-GPG-KEY-puppetlabs':
-      path => '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
-    }
+    if $::osfamily == 'RedHat' {
+      include puppetlabs_yum::products
+      include puppetlabs_yum::deps
+      class { 'puppetlabs_yum::devel':
+        enable_devel   => $enable_devel,
+      }
 
-    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs':
-      ensure => present,
-      owner  => 0,
-      group  => 0,
-      mode   => '0644',
-      source => 'puppet:///modules/puppetlabs_yum/RPM-GPG-KEY-puppetlabs',
-    }
+      puppetlabs_yum::rpm_gpg_key{ 'RPM-GPG-KEY-puppetlabs':
+        path => '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+      }
 
-  } else {
-      notice ("Your operating system ${::operatingsystem} will not have Puppet Labs Yum repository configured")
+      file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs':
+        ensure => present,
+        owner  => 0,
+        group  => 0,
+        mode   => '0644',
+        source => 'puppet:///modules/puppetlabs_yum/RPM-GPG-KEY-puppetlabs',
+      }
+
+      } else {
+        notice ("Your operating system ${::operatingsystem} will not have Puppet Labs Yum repository configured")
+      }
+  }
+  else {
+    notice('Not installing puppetlabs_yum because you have Puppet Enterprise installed')
   }
 
 }
